0014685da900049a98a664ad8f70fa0a9b8a34f7
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2022-02-13 12:09:36 +0800

    add active on selected event

--- src/app/modules/menu/menu.component.ts
+++ src/app/modules/menu/menu.component.ts
@@ -1,23 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
+import { url } from 'inspector';
 import { CardService } from 'src/app/services/card.service';
+
+export class Menu{
+  public name: string;
+  public active: boolean;
+  constructor(_name: string, _active: boolean){
+    this.name = _name;
+    this.active = _active;
+  }
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
+
 export class MenuComponent implements OnInit {
+  event?: string;
   service: CardService;
-  categories: string[] = ['All'];
+  router: Router;
+  categories: Menu[] = [new Menu('All', false)];
 
   constructor(
-    private _service: CardService
+    private _service: CardService,
+    private _activateRoute: ActivatedRoute,
+    private _router: Router
   ) { 
     this.service = _service;
+    this.router = _router;
   }
 
   ngOnInit(): void {
-    this.loadCategories();
+    this.router.events.subscribe(event =>{
+      if (event instanceof NavigationStart){
+        if (event['url'].includes('/events/')){
+          this.event = event['url'].replace('/events/', '').replace('%20', ' ');
+        }
+
+        if ((!this.event) || (this.event == "All")){
+          this.categories[0].active = true;
+        }
+        this.loadCategories();
+      }
+    });
   }
 
   loadCategories(){
@@ -30,19 +58,19 @@ export class MenuComponent implements OnInit {
           })
         }
       });
-      console.log(this.categories);
+      //console.log(this.categories);
     });
   }
 
   addCategory(category: string){
     let isFound: boolean = false;
     this.categories.forEach(cat => {
-      if (cat == category.trim()){
+      if (cat.name == category.trim()){
         isFound = true;
-      }
+      }``
     })
     if (!isFound){
-      this.categories.push(category.trim());
+      this.categories.push(new Menu(category.trim(), this.event == category.trim()));
     }
   }
 
