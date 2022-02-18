8f1e6eca6acf52977f457c790a415421de80c227
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2022-02-18 13:07:36 +0800

    Item Pagination

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -2,6 +2,21 @@ import { Card } from './../models/card';
 import { CardService } from './../services/card.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { throws } from 'assert';
+
+export class Page
+{
+  public index: number;
+  public start: number;
+  public end: number;
+  public display: string;
+  public showing: string;
+  public selected: boolean;
+
+  constructor(_index: number){
+    this.index = _index;
+  }
+}
 
 @Component({
   selector: 'app-cards',
@@ -17,6 +32,15 @@ export class CardsComponent implements OnInit {
   activateRoute: ActivatedRoute;
 
   cards: Card[] = [];
+  displayCards: Card[] = [];
+  pages: Page[] = [];
+  index: number;
+  batchLimit: number = 40;
+  batchCount: number = 0;
+  batchShowing: string = '';
+  disablePrev: boolean;
+  disableNext: boolean;
+
 
   constructor(
     private _service: CardService,
@@ -53,6 +77,8 @@ export class CardsComponent implements OnInit {
   loadAll(){
     this.service.getCards().then(data => {
       this.cards = data;
+      this.initializeBatch();
+      this.loadBatch(1);
     });
   }
 
@@ -66,6 +92,8 @@ export class CardsComponent implements OnInit {
             }
           })
         }
+        this.initializeBatch();
+        this.loadBatch(1);
       });
     });
   }
@@ -86,7 +114,72 @@ export class CardsComponent implements OnInit {
           this.cards.push(card);
         }
       });
+      this.initializeBatch();
+      this.loadBatch(1);
     });
   }
 
+  initializeBatch(){
+    if (this.cards.length > this.batchLimit){
+      this.batchCount = Math.trunc(this.cards.length / this.batchLimit);
+      if (this.batchCount < (this.cards.length / this.batchLimit)){
+        this.batchCount++;
+      }
+    }
+    else{
+      this.batchCount = 1;
+    }
+
+    for(let i = 1; i <= this.batchCount; i++){
+      let page: Page = new Page(i);
+      page.end = i * this.batchLimit;
+      if (page.end > this.cards.length)
+        page.end = this.cards.length;
+      page.start = page.end - (this.batchLimit - 1);
+      page.display = `Page ${i} of ${this.batchCount}`;
+      page.showing = `Showing ${page.start} - ${page.end} to ${this.cards.length} items`;
+      this.pages.push(page);
+    }
+  }
+
+  loadBatch(_index: number){
+    this.index = _index;
+    this.pages.forEach(page => {
+      if (page.index == this.index){
+        page.selected = true;
+        this.displayCards = [];
+        for(let i = page.start - 1; i <= page.end - 1; i++){
+          this.displayCards.push(this.cards[i]);
+        }
+        this.batchShowing = page.showing;
+      }
+      else{
+        page.selected = false;
+      }
+    });
+    if (this.index == 1){
+      this.disablePrev = false;
+      this.disableNext = true;
+    }
+    else if (this.index == this.batchCount){
+      this.disablePrev = true;
+      this.disableNext = false;
+    }
+    else{
+      this.disablePrev = true;
+      this.disableNext = true;
+    }
+  }
+
+  changeSelected(event: any){
+    this.loadBatch(+event.target.value);
+  }
+
+  clickNext(){
+    this.loadBatch(this.index + 1);
+  }
+
+  clickPrev(){
+    this.loadBatch(this.index - 1);
+  }
 }
