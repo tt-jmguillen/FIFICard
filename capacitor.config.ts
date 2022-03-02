8ec4338f1dacd362ad2c064bc218c83f10ac61cf
vincentcalaor <vincent.calaor@techtaxiinc.com>
2022-03-02 21:21:47 +0800

    ui fixes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'fifi-greetings',
+  appName: 'FibeiGreetings',
   webDir: 'www',
   bundledWebRuntime: false
 };
