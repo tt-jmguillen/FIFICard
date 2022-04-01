86d9e2d4f984999214d30fae183fc96995700c41
CharlottEscuro <charlotte.escuro@techtaxiinc.com>
2022-04-01 10:22:44 +0800

    Remove TabsPages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,12 +12,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TabsComponent } from './pages/tabs/tabs.component';
-import { TabEventsComponent } from './pages/tab-events/tab-events.component';
-import { TabGiftsComponent } from './pages/tab-gifts/tab-gifts.component';
-import { TabLoginComponent } from './pages/tab-login/tab-login.component';
-import { TabCreationsComponent } from './pages/tab-creations/tab-creations.component';
-import { TabStickersComponent } from './pages/tab-stickers/tab-stickers.component';
-import { TabProfileComponent } from './pages/tab-profile/tab-profile.component';
 import { ProfileComponent } from './settings/profile/profile.component';
 
 const routes: Routes = [
@@ -25,28 +19,7 @@ const routes: Routes = [
     path: '', component: HomeComponent
   },
   {
-    path: 'tabs',
-    component: TabsComponent,
-    children: [
-      {
-        path: 'events', component: TabEventsComponent
-      },
-      {
-        path: 'gifts', component: TabGiftsComponent
-      },
-      {
-        path: 'login', component: TabLoginComponent
-      },
-      {
-        path: 'profile', component: TabProfileComponent
-      },
-      {
-        path: 'creations', component: TabCreationsComponent
-      },
-      {
-        path: 'stickers', component: TabStickersComponent
-      },
-    ]
+    path: 'tabs', component: TabsComponent
   },
   {
     path: 'profile', component: ProfileComponent
