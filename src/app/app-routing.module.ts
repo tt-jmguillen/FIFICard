91e26664a484f5fac70b0711211243388d9024d2
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2022-04-29 20:54:50 +0800

    Add recipient parameter for events

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -55,6 +55,9 @@ const routes: Routes = [
     path: 'cards/events/:event', component: CardsComponent
   },
   {
+    path: 'cards/events/:event/:recipient', component: CardsComponent
+  },
+  {
     path: 'search/:search', component: CardsComponent
   },
   {
