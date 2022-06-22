b4a6cea3eb831c1056ec079d68ccfcb2e071f703
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2022-06-23 03:54:42 +0800

    Fix Shipping fee for added items

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -327,6 +327,7 @@ export class OrderComponent implements OnInit {
     this.createAnOrder(this.order).then(id => {
       this.addMore.forEach(item => {
         if (item.count > 0) {
+          console.log(item);
           let order: Order = new Order();
           order.card_id = item.card.id;
           order.card_price = item.card.price;
@@ -334,7 +335,7 @@ export class OrderComponent implements OnInit {
           order.parentOrder = id;
           order.shipping_fee = item.shipping_fee;
           order.user_id = this.uid;
-
+          console.log(order);
           this.createAnAddMoreOrder(order).then(_id => {
             //console.log(_id);
           });
