bb1662e7c3f38c6025747be63076fffa37af98de
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2022-10-13 18:45:17 +0800

    Gifts Landing Page
    Update Gifts landing page with new grouping

--- src/app/gifts/gifts.component.ts
+++ src/app/gifts/gifts.component.ts
@@ -8,11 +8,15 @@ import { Event } from '../models/event';
   styleUrls: ['./gifts.component.scss']
 })
 export class GiftsComponent implements OnInit {
-  
+
   constructor() { }
+
+  mode: 'Category' | 'Recipient' | 'Event' = 'Category';
 
   ngOnInit(): void {
   }
 
-  
+  modeChange(mode: 'Category' | 'Recipient' | 'Event') {
+    this.mode = mode;
+  }
 }
