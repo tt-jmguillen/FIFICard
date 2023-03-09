1bf171a78dcbb97e8d148f97ce9a46c8e3563438
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2023-03-09 21:48:47 +0800

    Video Playing
    Change Preview to main video player on Detail and Order Page

--- src/app/modules/ecard-image-grid/ecard-image-grid.component.ts
+++ src/app/modules/ecard-image-grid/ecard-image-grid.component.ts
@@ -1,9 +1,9 @@
-import { LightboxComponent } from './../lightbox/lightbox.component';
-import { LightboxImage } from './../lightbox/lightbox-image';
+import { Router } from '@angular/router';
 import { ImageService } from 'src/app/services/image.service';
 import { CardService } from 'src/app/services/card.service';
-import { Component, OnInit, Input, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef, OnDestroy, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { ECardImage } from 'src/app/models/ecard-image';
+import { Page } from '@ionic/core';
 
 @Component({
   selector: 'app-ecard-image-grid',
@@ -12,46 +12,48 @@ import { ECardImage } from 'src/app/models/ecard-image';
 })
 export class ECardImageGridComponent implements OnInit {
   @Input() id: string;
-  @ViewChild('lightbox') lightbox: LightboxComponent;
+
+  @ViewChild('player') player: ElementRef;
 
   def: ChangeDetectorRef;
   cardService: CardService;
   imageService: ImageService;
+  router: Router
 
   constructor(
     _def: ChangeDetectorRef,
     _cardService: CardService,
-    _imageService: ImageService
+    _imageService: ImageService,
+    _router: Router
   ) { 
     this.def = _def;
     this.cardService = _cardService;
     this.imageService = _imageService;
+    this.router = _router;
   }
 
   image: ECardImage;
-  lightboxImages: LightboxImage[] = [];
   url: string = '';
 
   ngOnInit(): void {
     this.loadImage();
+
+    this.router.events.subscribe((val) => {
+      this.player.nativeElement.pause();
+     });
   }
 
   loadImage(){
     this.cardService.getECardImages(this.id).then(images => {
-      this.image = images.find(x => x.title == 'preview')!;
+      this.image = images.find(x => x.title == 'video')!;
       this.def.detectChanges();
       if (this.image){
         this.imageService.getImageURL(this.image.url).then(url => {
           this.url = url;
           this.def.detectChanges();
-          this.lightboxImages.push(new LightboxImage(1, url, 'Preview'));
         })
       }
     })
-  }
-
-  openItem() {
-    this.lightbox.open(0);
   }
 
 }
